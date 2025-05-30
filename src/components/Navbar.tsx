624148aef424b0e192c8ebdd6277d16a21bbc77e
DhruvP <dhruvpareek883@gmail.com>
2025-05-29 17:23:56 -0700

    Login/Logout All working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+  const loginButton = <button onClick={() => loginWithRedirect()}>Log In</button>;
+  const logoutButton = <button className="logout-button" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>;
+
   return (
     <nav className="navbar">
       <ul className="navbar-list">
@@ -18,10 +23,15 @@ const Navbar = () => {
             Select Teams
           </Link>
         </li>
-        <li>
-            <Link to="/sign-in" className="navbar-link">
-                Sign In
-            </Link>
+        <li className="user-menu-container">
+            {isAuthenticated ? (
+              <>
+                <span>{user?.name}</span>
+                {logoutButton}
+              </>
+            ) : (
+              loginButton
+            )}
         </li>
       </ul>
     </nav>
